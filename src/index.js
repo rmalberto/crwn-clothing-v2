baa0742f26601f3658ac30a521bbbc9358cbbd73
Alberto Rodarte <arodarte@arodarte.local>
2022-05-28 21:42:17 -0500

    fix: import home.component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.scss';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import './index.scss';
+import App from './App';
 const rootElement = document.getElementById('root');
 
 render(
