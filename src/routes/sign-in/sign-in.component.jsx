7f48007ae6eb7b26a79224acb822972ca8ecf946
rmalberto <arodarte.deacero.com>
2022-09-11 15:38:47 -0500

    lesson 97 -Sign up form pt1

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -8,6 +8,8 @@ import {
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
 const SignIn = () => {
   
   // useEffect(async () => {
@@ -45,6 +47,7 @@ const SignIn = () => {
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
       <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+      <SignUpForm />
     </div>
   );
 };
