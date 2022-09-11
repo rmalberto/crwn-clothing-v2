f3cdf9b1ef7afe10f2f20cae34f6389b0c73e1d8
rmalberto <arodarte.deacero.com>
2022-09-11 14:58:23 -0500

    lesson 96 - Redirect authentication with google

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,50 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+  
+  // useEffect(async () => {
+  //   const response = await getRedirectResult(auth);
+  //   console.log(response);
+  //   if(response){
+  //     const userDocRef = await createUserDocumentFromAuth(response.user);    
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getRedirectResult(auth);
+      //console.log(response);
+      if(response){
+        const userDocRef = await createUserDocumentFromAuth(response.user);    
+      }
+    }
+    fetchData();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
+  // const logGoogleRedirectUser = async () => {
+  //   const { user } = await signInWithGoogleRedirect();
+  //   console.log(user);
+  //   const userDocRef = await createUserDocumentFromAuth(user);
+  // };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
     </div>
   );
 };
