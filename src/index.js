7aeb3a6403969c082f572c4ee440aba0cf6988a9
rmalberto <arodarte.deacero.com>
2022-12-11 00:23:16 -0600

    feat: lesson 113

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 
 import "./index.scss";
 
@@ -33,7 +34,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
